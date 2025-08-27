1bd18360bbe44baa5dd58844aba78d286d4b2d9b
umangchauhan <umang4191@gmail.com>
2025-08-27 20:24:00 +0200

    Read comment to see the reason.

--- ValidationPract/Program.cs
+++ ValidationPract/Program.cs
@@ -4,6 +4,15 @@ Console.WriteLine("Start!");
 
 Employee emp = new();
 
+// This is working because C# is assigning default values to properties
+// of the object emp. And our validation gets triggered only when we manually
+// set some value for the property.
+// In this case, our validation is not even getting triggered.
+
+// To fix this or enforce this "required" check on FirstName, we can 
+// 1. use required keyword
+// 2. use constructor
+
 // try
 // {
 //     emp.FirstName = "John";
